c496fd037b0b03edbfc4424d5756d91058ff9e58
jplande <lalandejeanpaul10@gmail.com>
2025-11-22 11:57:02 +0100

    Initialisation back-end springboot

---
+++ src/main/java/com/transitea/TransiteaApplication.java
@@ -0,0 +1,13 @@
+package com.transitea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransiteaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransiteaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transitea/TransiteaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transitea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransiteaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
